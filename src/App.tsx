d3e60721fdf964ffd03285ebfdff99f697afd601
ANSI Rasslen <rasslen.ansi@carnet.tn>
2024-05-10 23:35:01 +0100

    Tp1 Test

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+type Priority = 'p1' | 'p2' | 'p3';
+
+type Task = {
+  id: number;
+  title: string;
+  isCompleted: boolean;
+  priority?: Priority;
+};
+
+function App() {
+    const [tasks, setTasks] = React.useState<Task[]>([]);
+
+
+    const [taskName, setTaskName] = React.useState('');
+
+  const onAddTask = () => {
+      const trimmedTaskName = taskName.trim();
+
+
+      if (!trimmedTaskName) {
+          return;
+      }
+
+      setTasks([
+      ...tasks,
+      {
+        id: new Date().getTime(), // Not a great way to generate IDs
+          title: trimmedTaskName,
+          isCompleted: false,
+      },
+    ]);
+      setTaskName('');
+  };
+
+  return (
+    <div>
+      <h1>Tasks</h1>
+      <label htmlFor="task-input">Add Task: </label>
+      <input
+        id="task-input"
+        value={taskName}
+        onChange={(e) => setTaskName(e.target.value)}
+      />
+      <button onClick={onAddTask}>Add</button>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>{task.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
